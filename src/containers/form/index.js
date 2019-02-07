97e682c01decf29d37d1c8bc84ed7118bed889bf
Lykov Roman <rslykov@dasreda.ru>
2019-02-07 15:10:01 +0300

    add base containers

---
+++ src/containers/form/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class Form extends Component {
+  render() {
+    return <form>
+        <h1>Редактирование заявления</h1>
+    </form>;
+  }
+}
+
+Form.propTypes = {};
+Form.defaultProps = {};
+
+export default Form;
---
+++ src/containers/list/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+class List extends Component {
+  render() {
+    return <form>
+        <h1>Список заявлений</h1>
+    </form>;
+  }
+}
+
+List.propTypes = {};
+List.defaultProps = {};
+
+export default List;
