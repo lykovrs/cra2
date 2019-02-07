41900599d02f867682b1829e48891c0e96b74167
Lykov Roman <rslykov@dasreda.ru>
2019-02-07 15:37:21 +0300

    add base components

---
+++ src/components/List/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function DraftList(props) {
+    return (
+        <div>Список</div>
+
+    );
+}
+
+DraftList.propTypes = {};
+DraftList.defaultProps = {};
+
+export default DraftList;
--- src/containers/list/index.js
+++ src/containers/list/index.js
@@ -1,15 +1,37 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import DraftList from '../../components/List'
 
 class List extends Component {
+
   render() {
-    return <form>
-        <h1>Список заявлений</h1>
-    </form>;
+    const { title, draftList } = this.props;
+    return (
+      <form>
+        <h1>{title}</h1>
+
+        <DraftList items={draftList}/>
+      </form>
+    );
   }
 }
 
-List.propTypes = {};
-List.defaultProps = {};
+List.propTypes = {
+  title: PropTypes.string,
+    draftList: PropTypes.array,
+};
+List.defaultProps = {
+  title: "Список заявлений",
+    draftList: [
+        {title: 'Черновик 1',
+            firstStatus: 'First status',
+            secondStatus: 'Second status',
+        },
+        {title: 'Черновик 1',
+            firstStatus: 'First status',
+            secondStatus: 'Second status',
+        },
+    ]
+};
 
 export default List;
